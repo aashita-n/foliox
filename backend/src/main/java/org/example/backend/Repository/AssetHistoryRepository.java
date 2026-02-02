33c3577b618e0ddab2cd878bb069f08742b83323
Aradhya-S <aradhya_sh@hotmail.com>
2026-02-02 19:12:06 +0000

    added the api to see the historic data of the stock by there ticker

--- backend/src/main/java/org/example/backend/Repository/AssetHistoryRepository.java
+++ backend/src/main/java/org/example/backend/Repository/AssetHistoryRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface AssetHistoryRepository extends JpaRepository<AssetHistoryEntity, Long> {
     List<AssetHistoryEntity> findBySymbolOrderByDateAsc(String symbol);
+
 }
 
 
